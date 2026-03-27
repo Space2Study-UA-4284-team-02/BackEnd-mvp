478a0f4598dcd05ae277c9d58e4d90a66a090f26
Anna Romanovska <sup2a1nn@gmail.com>
2026-03-27 15:02:55 +0200

    fix: support single and multiple categories in query using

--- src/utils/getCategoriesOption.js
+++ src/utils/getCategoriesOption.js
@@ -3,6 +3,10 @@ const getCategoriesOptions = (categories) => {
 
   const categoriesArray = Array.isArray(categories) ? categories : [categories]
 
-  return categoriesArray.map((item) => (item === 'null' ? null : item))
+  const normalizedCategories = categoriesArray.map((item) => (item === 'null' ? null : item))
+  if (normalizedCategories.length === 1) {
+    return normalizedCategories[0]
+  }
+  return { $in: normalizedCategories }
 }
 module.exports = getCategoriesOptions
